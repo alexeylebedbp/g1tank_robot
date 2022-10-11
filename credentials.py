e3a5dd308fdf613fb558357c6af7c68b8ce64a58
alexeylebedbp <alexey.lebed@brightpattern.com>
2022-10-11 16:57:29 -0700

    add frequency cred

--- credentials.py
+++ credentials.py
@@ -2,5 +2,6 @@ credentials = {
     "host": 'ws://10.0.0.250:8080',
     "car_id": "811BF759-11DE-4729-83B2-3408B80208C5",
     "initial_reconnect_timeout": "5",
-    "max_reconnect_timeout": "20"
+    "max_reconnect_timeout": "20",
+    "motor_frequency": "5" #Hz
 }
